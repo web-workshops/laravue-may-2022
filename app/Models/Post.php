c8bf7e95e81f3713670f3245ec558b32763917ca
houcem <hedhoucem@gmail.com>
2022-06-02 12:27:57 +0100

    Comment / Post models relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\Comment;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -19,5 +20,10 @@ class Post extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
     }
 }
