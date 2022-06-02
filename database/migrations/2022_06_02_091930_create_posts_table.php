1db01882eb56cb42801cbcb3c234c23317bb62d6
houcem <hedhoucem@gmail.com>
2022-06-02 10:57:21 +0100

    write migrations

--- database/migrations/2022_06_02_091930_create_posts_table.php
+++ database/migrations/2022_06_02_091930_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,16 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->string('image')->nullable();
+            // $table->unsignedBigInteger('category_id');
+            $table->foreignId('category_id');
+            // $table->bigInteger('user_id')->unsigned();
+            $table->foreignIdFor(Category::class);
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
--- database/migrations/2022_06_02_091942_create_comments_table.php
+++ database/migrations/2022_06_02_091942_create_comments_table.php
@@ -15,7 +15,12 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->string('comment');
+            $table->foreignId('post_id');
+            $table->foreignId('user_id');
             $table->timestamps();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
