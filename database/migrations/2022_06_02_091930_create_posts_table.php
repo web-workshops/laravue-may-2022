6a1ea9e43973fb7225e0e5bf74ffe22a19cfb084
houcem <hedhoucem@gmail.com>
2022-06-02 12:01:52 +0100

    fix User::call import

--- database/migrations/2022_06_02_091930_create_posts_table.php
+++ database/migrations/2022_06_02_091930_create_posts_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -22,7 +23,7 @@ class CreatePostsTable extends Migration
             // $table->unsignedBigInteger('category_id');
             $table->foreignId('category_id');
             // $table->bigInteger('user_id')->unsigned();
-            $table->foreignIdFor(Category::class);
+            $table->foreignIdFor(User::class);
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade')->onUpdate('cascade');
