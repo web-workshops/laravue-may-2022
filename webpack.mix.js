013e0a5ef52c7439fbdb7e300b772146df9ec8e4
houcem <hedhoucem@gmail.com>
2022-06-03 12:30:46 +0100

    php artisan breeze:install

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require("laravel-mix");
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,9 +11,7 @@ const mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/js/app.js", "public/js")
-    .styles(["resources/css/blog.css"], "public/css/blog.css")
-    .postCss("resources/css/app.css", "public/css", [
-        //
-    ])
-    .sass("resources/sass/app.scss", "public/css");
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+    require('autoprefixer'),
+]);
